3ea5de76c047e1ee58ef7e405bddf658db06ac0e
Ricardo <SH_Ricardo@hotmail.com>
2022-10-05 19:42:41 -0300

    theme context

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import * as bootstrap from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Switch from "./Components/Switch.js";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <div>
+  <div className="bg-dark">
+    <Switch />
     <App />
   </div>
 );
